0445b2a30dc9c50fefd2af1baf00cc42f940378c
LeroyAndrade <leroy_andrade@hotmail.com>
2022-11-24 02:37:28 +0100

    + config zodat alle wijzigingen snel gedaan kunnen worden + de images zoeken nu naar $conf->uploadImg, hierdoor kun je bij een db migratie snel 1 variabel aanpassen waardoor overal de wijziging is doorgevoerd.

---
+++ config/includeConfig.php
@@ -0,0 +1,27 @@
+<?php
+
+class globalConfigs {
+
+    public $baseurl = '/';
+    public $uploadurl = 'resources/';
+    public $uploadImg = 'resources/images/';
+
+    public $thumbw = 450;
+    public $thumbh = 450;
+    private $cmsurl = 'cms';
+    public $trailingSlash = true;
+
+    public $title = 'Fires of destruction';
+    public $template = 'FoD';
+    public $emailto = '@firesofdestruction.com';
+    public $emailfrom = 'Fires of destruction <info@firesofdestruction.com>';
+    public $breadcrumbs = true;
+
+
+    public $imageSignature = 'Fires of Destruction';
+}
+
+//return [
+//    "namee" => "newBlog
+//root = base_path();
+//];
--- resources/views/productpagina.blade.php
+++ resources/views/productpagina.blade.php
@@ -1,23 +1,46 @@
+<?php
+
+$conf = new globalConfigs();
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="123 Lens, 123, lens, bril, brillen, lens, lenzen, zien, opticien, hans anders, pearl, sterkte">
+    <meta name="author" content="Leroy Andrade">
+    <title>123 Lens opdracht</title>
+
+    @vite(['resources/css/productPaginaShow.css', 'resources/js/app.js'])
 
 </head>
 
 
     <body>
-Pagina bereikt
+    Pagina bereikt
     {{$productId}}
-    <?php
-        foreach ($productMetId as $productKey=>$val) : ?>
-            <img class="imageBig" src="{{ Vite::asset('resources/images/'.$val->imageBig) }}" alt="<?php echo $val->{'title'} ?> ">
+
+    <section class="productPaginaContainer">
+
+        <article class="productShow">
+
             <?php
-    //            echo $val->title;
-        endforeach; ?>
+
+
+
+                foreach ($productMetId as $productKey=>$val) : ?>
+                    <img class="imageBig" src="{{ Vite::asset($conf->uploadImg.$val->imageBig) }}" alt="<?php echo $val->{'title'} ?> ">
+                    <?php
+            //            echo $val->title;
+                    echo $val->description;
+                    dd($val);
+                endforeach; ?>
+        </article>
+
+    </section>
+
     </body>
 </html>
